ddfff37aa95bdfed438ae857c8777fbb253dff07
Vijaysunder21 <97424648+Vijaysunder21@users.noreply.github.com>
2022-05-30 11:56:16 +0530

    n=int(input()) arr=list(map(int,input().strip().split())) m=max(arr) for i in range(m,0,-1):     c=0     for j in range(n):         if(arr[j]%i==0):             c=c+1     if(c==n):         print(i)         break

---
+++ Minimum_Scale_length.py
@@ -0,0 +1,13 @@
+n=int(input())
+arr=list(map(int,input().strip().split()))
+m=max(arr)
+for i in range(m,0,-1):
+    c=0
+    for j in range(n):
+        if(arr[j]%i==0):
+            c=c+1
+    if(c==n):
+        print(i)
+        break
+    
+    
