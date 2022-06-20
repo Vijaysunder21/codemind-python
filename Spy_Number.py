03ca642ed8981cb24a5909e388f47842e39801be
Vijaysunder21 <97424648+Vijaysunder21@users.noreply.github.com>
2022-06-20 20:09:49 +0530

    n=int(input()) k=list(str(n)) s=0 p=1 for i in range(len(k)):     s=s+int(k[i])     p=p*int(k[i]) if(s==p):     print("Spy Number") else:     print("Not Spy Number")

--- Spy_Number.py
+++ Spy_Number.py
@@ -1,13 +1,13 @@
 n=int(input())
+k=list(str(n))
 s=0
 p=1
-while(n>0):
-    d=n%10
-    s=s+d
-    p=p*d
-    n=n//10
+for i in range(len(k)):
+    s=s+int(k[i])
+    p=p*int(k[i])
 if(s==p):
-    print('Spy Number')
+    print("Spy Number")
 else:
-    print('Not Spy Number')
+    print("Not Spy Number")
+    
     
