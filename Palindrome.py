707ed92e028c7aaa49d020f1ee03142d991e90cc
Vijaysunder21 <97424648+Vijaysunder21@users.noreply.github.com>
2022-06-20 22:30:02 +0530

    def pal(n):     s=0     temp=n     while(n!=0):         d=n%10         s=s*10+d         n=n//10     if(s==temp):         return 1     else:         return 0 n=int(input()) if(pal(n)):     print("True") else:     print("False")

--- Palindrome.py
+++ Palindrome.py
@@ -1,11 +1,16 @@
+def pal(n):
+    s=0
+    temp=n
+    while(n!=0):
+        d=n%10
+        s=s*10+d
+        n=n//10
+    if(s==temp):
+        return 1
+    else:
+        return 0
 n=int(input())
-temp=n
-s=0
-while(n):
-    d=n%10
-    s=s*10+d
-    n=n//10
-if(s==temp):
-    print('True')
+if(pal(n)):
+    print("True")
 else:
-    print('False')
+    print("False")
