ea6a6ef4c9456f74b26a39d9166f534030c0bb2c
Vijaysunder21 <97424648+Vijaysunder21@users.noreply.github.com>
2022-06-20 22:12:58 +0530

    def gcd(a,b):     if a>b:         s=b     else:         s=a     for i in range(1,s+1):         if(a%i==0 and b%i==0):             h=i     return h a,b=map(int,input().split()) print(gcd(a,b))

---
+++ GCD_or_HCF.py
@@ -0,0 +1,11 @@
+def gcd(a,b):
+    if a>b:
+        s=b
+    else:
+        s=a
+    for i in range(1,s+1):
+        if(a%i==0 and b%i==0):
+            h=i
+    return h
+a,b=map(int,input().split())
+print(gcd(a,b))
