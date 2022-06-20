6db199e3b251210d5bc15aebabf66adecd639aab
Vijaysunder21 <97424648+Vijaysunder21@users.noreply.github.com>
2022-06-20 20:12:39 +0530

    n=int(input()) k=n*n l=list(str(k)) s=0 for i in range(len(l)):     s=s+int(l[i]) if(s==n):     print("Neon Number") else:     print("Not Neon Number")

--- Neon_Number.py
+++ Neon_Number.py
@@ -1,11 +1,10 @@
 n=int(input())
-s=0
 k=n*n
-while(k>0):
-    d=k%10
-    s=s+d
-    k=k//10
+l=list(str(k))
+s=0
+for i in range(len(l)):
+    s=s+int(l[i])
 if(s==n):
-    print('Neon Number')
+    print("Neon Number")
 else:
-    print('Not Neon Number')
+    print("Not Neon Number")
