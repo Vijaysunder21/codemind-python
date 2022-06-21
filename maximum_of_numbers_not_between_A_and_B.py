db7e5106752f4f43ada35cac35d9ff396f72035e
Vijaysunder21 <97424648+Vijaysunder21@users.noreply.github.com>
2022-06-21 20:08:53 +0530

    n=int(input()) arr=list(map(int,input().split())) a,b=map(int,input().split()) k=0 s=0 for i in range(len(arr)):     k=0     if(arr[i]>=a and arr[i]<=b):         k=1     if(k==0):         s=1         print(max(arr))         break if(s==0):     print("-1")

--- maximum_of_numbers_not_between_A_and_B.py
+++ maximum_of_numbers_not_between_A_and_B.py
@@ -1,13 +1,16 @@
 n=int(input())
 arr=list(map(int,input().split()))
-a,c=map(int,input().split())
-b=[]
-f=0
-for i in range(n):
-    if arr[i]<a or arr[i]>c:
-        f=1
-        b.append(arr[i])
-if(f==0):
+a,b=map(int,input().split())
+k=0
+s=0
+for i in range(len(arr)):
+    k=0
+    if(arr[i]>=a and arr[i]<=b):
+        k=1
+    if(k==0):
+        s=1
+        print(max(arr))
+        break
+if(s==0):
     print("-1")
-else:
-    print(max(b))
+    
