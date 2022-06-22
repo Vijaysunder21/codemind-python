adc6dab2c0a6166521550fff1adcbbd34790a2af
Vijaysunder21 <97424648+Vijaysunder21@users.noreply.github.com>
2022-06-22 22:15:04 +0530

    s=input() v=list("aeiou") for i in s:     if i in v:         v.remove(i) if(len(v)==0):     print("0") else:     print(*v)

--- vowels_not_in_a_string.py
+++ vowels_not_in_a_string.py
@@ -1,13 +1,9 @@
 s=input()
 v=list("aeiou")
-b=[]
 for i in s:
-    if i in "aeiou":
-        b.append(i)
-for i in v:
-    if i not in b:
-        print(i,end=" ")
-b=set(b)
-if(len(b)==len(v)):
+    if i in v:
+        v.remove(i)
+if(len(v)==0):
     print("0")
-        
+else:    
+    print(*v)        
