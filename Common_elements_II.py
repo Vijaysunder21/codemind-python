1e8d9b791523f97540393a6397a85e2971fcb17e
Vijaysunder21 <97424648+Vijaysunder21@users.noreply.github.com>
2022-07-01 22:23:51 +0530

    a,b=map(int,input().split()) arr=list(map(int,input().split())) brr=list(map(int,input().split())) c=[] d=[] for i in arr:     if i not in brr:         c.append(i) for i in brr:     if i not in arr:         c.append(i) print(*c)

--- Common_elements_II.py
+++ Common_elements_II.py
@@ -1,19 +1,13 @@
-n,m=map(int,input().split())
-a=list(map(int,input().split()))
-b=list(map(int,input().split()))
+a,b=map(int,input().split())
+arr=list(map(int,input().split()))
+brr=list(map(int,input().split()))
 c=[]
 d=[]
-k=[]
-for i in a:
-    if i not in c:
+for i in arr:
+    if i not in brr:
         c.append(i)
-for i in b:
-    if i not in d:
-        d.append(i)
-for i in c:
-    if i not in d:
-        k.append(i)
-for i in d:
-    if i not in c:
-        k.append(i)
-print(*k)        
+for i in brr:
+    if i not in arr:
+        c.append(i)
+print(*c)        
+            
