1b8299535d3579e4674fa9911acbfea3fb4473dd
Vijaysunder21 <97424648+Vijaysunder21@users.noreply.github.com>
2022-08-29 20:31:16 +0530

    r,c=map(int,input().split()) m=[list(map(int,input().split())) for i in range(r)] s=0 for i in range(r):     for j in range(c):         if(i==0 or j==0 or i==r-1 or j==r-1):             s=s+m[i][j] print(s)

---
+++ Sum_of_all_boundary_values_in_a_matrix.py
@@ -0,0 +1,9 @@
+r,c=map(int,input().split())
+m=[list(map(int,input().split())) for i in range(r)]
+s=0
+for i in range(r):
+    for j in range(c):
+        if(i==0 or j==0 or i==r-1 or j==r-1):
+            s=s+m[i][j]
+print(s)            
+        
