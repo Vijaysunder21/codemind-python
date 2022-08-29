2f5dc267718f2f32a4bbb70d2deb9dad6578820b
Vijaysunder21 <97424648+Vijaysunder21@users.noreply.github.com>
2022-08-29 11:31:19 +0530

    n=int(input()) arr=list(map(int,input().split())) a=0 b=0 for i in range(0,n-2,2):     if(arr[i]<arr[i+1]) and (arr[i+1]>arr[i+2]):         a=a+1     elif(arr[i]>arr[i+1] and (arr[i+1]<arr[i+2])):         a=a+1     else:         b=1 if(b==0):     print("yes") else:     print("no")

--- Wave_array.py
+++ Wave_array.py
@@ -3,9 +3,9 @@ arr=list(map(int,input().split()))
 a=0
 b=0
 for i in range(0,n-2,2):
-    if(arr[i]<arr[i+1] and arr[i+1]>arr[i+2]):
+    if(arr[i]<arr[i+1]) and (arr[i+1]>arr[i+2]):
         a=a+1
-    elif(arr[i]>arr[i+1] and arr[i+1]<arr[i+2]):
+    elif(arr[i]>arr[i+1] and (arr[i+1]<arr[i+2])):
         a=a+1
     else:
         b=1
